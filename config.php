3b1c0f225237f9f0ee91359f1ae9708636052e34
Hanter1 <pirogovds98@gmail.com>
2022-07-25 01:30:31 +0300

    first commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+defined('DMP_ACCESS') or die('Access denied');
+
+const SITE_URL = 'http://contentmanagementsystem';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'DMP';
